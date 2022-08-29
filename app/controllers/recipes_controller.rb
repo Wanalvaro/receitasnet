563cc5b5d51dc50c6257754d67b028c61a9c1ba9
Wanderson Alvaro <w.alvaro0909@gmail.com>
2022-08-29 10:41:41 -0300

    Commit inicial

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+class RecipesController < ApplicationController
+
+    def index
+       @recipes =['Strogonofe','Filé de frango a parmegiana','Salmão ao forno',
+    'Bacalhau ao Forno']
+
+  end 
+end
